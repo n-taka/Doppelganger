7d7f041077f996579075ea659e4ecded3d39b9e2
Kazutaka Nakashima <kazutaka.nakashima@n-taka.info>
2021-09-23 16:54:40 +0900

    [impl] loadPolygonMesh

--- plugin/loadPolygonMesh/module.js
+++ plugin/loadPolygonMesh/module.js
@@ -1,48 +1,48 @@
-import { DoppelCore } from '../../../js/DoppelCore.js';
-import { APIcall } from '../../../js/APIcall.js';
+import { UI } from '../../js/UI.js';
+import { getText } from '../../js/Text.js';
+import { request } from '../../js/request.js';
 
-var text = {
-    "Polygon": { "ja": "ポリゴン" },
-    "Import polygon": { "ja": "ポリゴン読み込み" }
+const text = {
+    "Polygon": { "en": "Polygon", "ja": "ポリゴン" },
+    "Import polygon": { "en": "Import polygon", "ja": "ポリゴン読み込み" }
 };
 
 ////
-// meta information
-////
-export var priority = 0;
-
-////
 // UI
-////
-export var generateUI = function () {
-    var topMenuUl = document.getElementById("topMenuUl");
-    var li = document.createElement("li");
-    topMenuUl.appendChild(li);
+const generateUI = async function () {
+    ////
+    // button
+    {
+        const li = document.createElement("li");
+        {
+            const a = document.createElement("a");
+            a.addEventListener('click', function () {
+                loadMeshes('.obj,.stl,.ply,.wrl');
+            });
+            a.setAttribute("class", "tooltipped");
+            a.setAttribute("data-position", "bottom");
+            a.setAttribute("data-tooltip", getText(text, "Import polygon"));
+            a.innerHTML = getText(text, "Polygon");
 
-    var a = document.createElement("a");
-    li.appendChild(a);
-    a.innerHTML = text["polygon"][DoppelCore.language in text["polygon"] ? DoppelCore.language : "default"];
-    a.addEventListener('click', function () {
-        loadMeshes('.obj,.stl,.ply,.wrl');
-    });
-    a.setAttribute("class", "tooltipped");
-    a.setAttribute("data-position", "bottom");
-    a.setAttribute("data-tooltip", text["tooltip"][DoppelCore.language in text["tooltip"] ? DoppelCore.language : "default"]);
-
-    var i = document.createElement("i");
-    a.appendChild(i);
-    i.innerHTML = "add";
-    i.setAttribute("class", "material-icons left");
-
+            {
+                const i = document.createElement("i");
+                i.innerHTML = "add";
+                i.setAttribute("class", "material-icons left");
+                a.appendChild(i);
+            }
+            li.appendChild(a);
+        }
+        UI.topMenuUl.appendChild(li);
+    }
 }
 
-function loadMeshes(formatToBeAccepted) {
+const loadMeshes = async function (formatToBeAccepted) {
     // http://pirosikick.hateblo.jp/entry/2014/08/11/003235
     if (loadMeshes.inputElement) {
         document.body.removeChild(loadMeshes.inputElement);
     }
 
-    var inputElement = loadMeshes.inputElement = document.createElement('input');
+    const inputElement = loadMeshes.inputElement = document.createElement('input');
 
     inputElement.type = 'file';
     inputElement.multiple = 'multiple';
@@ -59,7 +59,7 @@ function loadMeshes(formatToBeAccepted) {
     // https://qiita.com/weal/items/1a2af81138cd8f49937d
     inputElement.addEventListener('change', function (e) {
         if (e.target.files.length > 0) {
-            for (var file of e.target.files) {
+            for (let file of e.target.files) {
                 loadMesh(file);
             }
         }
@@ -67,34 +67,42 @@ function loadMeshes(formatToBeAccepted) {
     inputElement.click();
 }
 
-function loadMesh(file) {
-    var freader = new FileReader();
+const loadMesh = async function (file) {
+    const freader = new FileReader();
     freader.onload = function (event) {
+        const fileName = file.name;
+        const type = fileName.split('.');
+        const fileId = Math.random().toString(36).substring(2, 9);
+        const base64 = freader.result.substring(freader.result.indexOf(',') + 1);
+        // split into 0.5MB packets. Due to the default limit (up to 1MB) of boost beast.
+        const packetSize = 500000;
+        const packetCount = Math.ceil(base64.length / packetSize);
 
-        var fileName = file.name;
-        var type = fileName.split('.');
-        var fileId = Math.random().toString(36).substring(2, 9);
-
-        var base64 = freader.result.substring(freader.result.indexOf(',') + 1)
-        var packetSize = 1000000;
-        var packetCount = Math.ceil(base64.length / packetSize);
-
-        for (var packet = 0; packet < packetCount; ++packet) {
-            var base64Packet = base64.substring(packet * packetSize, (packet + 1) * packetSize);
-            var json = {
-                "sessionId": DoppelCore.sessionId,
-                "meshName": type.slice(0, -1).join("."),
-                "fileId": fileId,
-                "fileType": type[type.length - 1].toLowerCase(),
-                "packetIndex": packet,
-                "packetSize": packetSize,
-                "packetTotal": packetCount,
-                "byteTotal": base64.length,
-                "base64Packet": base64Packet
+        for (let packet = 0; packet < packetCount; ++packet) {
+            const base64Packet = base64.substring(packet * packetSize, (packet + 1) * packetSize);
+            const json = {
+                "mesh": {
+                    "name": type.slice(0, -1).join("."),
+                    "file": {
+                        "id": fileId,
+                        "size": base64.length,
+                        "packetId": packet,
+                        "packetSize": packetSize,
+                        "packetTotal": packetCount,
+                        "type": type[type.length - 1].toLowerCase(),
+                        "base64Packet": base64Packet
+                    }
+                }
             };
-
-            APIcall("POST", "api/updatePolygonMesh", JSON.stringify(json), "application/json");
+            request("loadPolygonMesh", json);
         }
     };
     freader.readAsDataURL(file);
 }
+
+////
+// UI
+export const init = async function () {
+    await generateUI();
+}
+
