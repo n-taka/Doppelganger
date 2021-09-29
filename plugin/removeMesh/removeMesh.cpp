045201a8715d989f3af90d48cb137446b2aa878e
Kazutaka Nakashima <kazutaka.nakashima@n-taka.info>
2021-09-29 13:42:31 +0900

    [impl] removeMesh

---
+++ plugin/removeMesh/removeMesh.cpp
@@ -0,0 +1,67 @@
+#ifndef TOGGLEMESHVISIBILITY_CPP
+#define TOGGLEMESHVISIBILITY_CPP
+
+#include "../pluginHeader.h"
+#include "Doppelganger/Room.h"
+#include "Doppelganger/triangleMesh.h"
+
+#include <string>
+#include <mutex>
+
+extern "C" DLLEXPORT void metadata(const std::shared_ptr<Doppelganger::Room> &room, const nlohmann::json &parameters, nlohmann::json &response, nlohmann::json &broadcast)
+{
+	response["author"] = "Kazutaka Nakashima";
+	response["version"] = "1.0.0";
+	broadcast = nlohmann::json::object();
+}
+
+extern "C" DLLEXPORT void pluginProcess(const std::shared_ptr<Doppelganger::Room> &room, const nlohmann::json &parameters, nlohmann::json &response, nlohmann::json &broadcast)
+{
+	////
+	// [IN]
+	// parameters = {
+	// 	"meshes": [
+	// 	 "UUID-A",
+	// 	 "UUID-B",
+	//   ...
+	//  ]
+	// }
+
+	// [OUT]
+	// response = {
+	// }
+	// broadcast = {
+	// 	"meshes" : {
+	//    "<meshUUID>": JSON object that represents the loaded mesh
+	//  }
+	// }
+
+	// create empty response/broadcast
+	response = nlohmann::json::object();
+	broadcast = nlohmann::json::object();
+	nlohmann::json diff = nlohmann::json::object();
+	nlohmann::json diffInv = nlohmann::json::object();
+
+	diff["meshes"] = nlohmann::json::object();
+	diffInv["meshes"] = nlohmann::json::object();
+	{
+		std::lock_guard<std::mutex> lock(room->mutexMeshes);
+
+		// remove mesh
+		for (const auto &UUID : parameters.at("meshes"))
+		{
+			const std::string &meshUUID = UUID.get<std::string>();
+			const std::shared_ptr<Doppelganger::triangleMesh> &mesh = room->meshes.at(meshUUID);
+			diffInv.at("meshes")[meshUUID] = mesh->dumpToJson(false);
+			diffInv.at("meshes").at(meshUUID)["remove"] = false;
+			// remove mesh (from here, shared_ptr "mesh" is invalidated.)
+			room->meshes.erase(meshUUID);
+			diff.at("meshes")[meshUUID] = nlohmann::json::object();
+			diff.at("meshes").at(meshUUID)["remove"] = true;
+			broadcast = diff;
+		}
+	}
+	room->storeHistory(diff, diffInv);
+}
+
+#endif
