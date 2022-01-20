ef1a6b39e593062ad6d922bcca8d44ce2f3000ff
Kazutaka Nakashima <kazutaka.nakashima@n-taka.info>
2022-01-21 03:23:58 +0900

    [impl] build scripts

--- build.sh
+++ build.sh
@@ -3,16 +3,16 @@
 ############
 # OS detection
 ############
-triplet="x"
+TRIPLET="x"
 if [ "$(uname)" == "Darwin" ]; then
     # we use custom triplet (x64-osx-mojave)
-    triplet="${triplet}64-osx-mojave"
+    TRIPLET="${TRIPLET}64-osx-mojave"
     # copy custom triplet file (for supporting Mojave)
-    cp "${triplet}.cmake" "submodule/vcpkg/triplets/${triplet}.cmake"
+    cp "${TRIPLET}.cmake" "submodule/vcpkg/triplets/${TRIPLET}.cmake"
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-    triplet="${triplet}64-windows"
+    TRIPLET="${TRIPLET}64-windows"
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    triplet="${triplet}64-linux"
+    TRIPLET="${TRIPLET}64-linux"
 else
     echo "This OS is not supported..."
     exit 1
@@ -21,24 +21,25 @@ fi
 ############
 # build project
 ############
+CONFIG="Debug"
 if [ "$(uname)" == "Darwin" ]; then
-    cmake -B build -S . -DVCPKG_TARGET_TRIPLET="${triplet}" -DCMAKE_BUILD_TYPE="Release" -DCMAKE_INSTALL_PREFIX=Doppelganger_install
-    cmake --build build --config "Release"
-    cmake --install build --config "Release"
+    cmake -B build -S . -DVCPKG_TARGET_TRIPLET="${TRIPLET}" -DCMAKE_BUILD_TYPE="${CONFIG}" -DCMAKE_INSTALL_PREFIX=Doppelganger_install
+    cmake --build build --config "${CONFIG}"
+    cmake --install build --config "${CONFIG}"
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-    # subst X: .
-    # cd X:
-    cmake -B build -S . -DVCPKG_TARGET_TRIPLET="${triplet}" -DCMAKE_BUILD_TYPE="Debug" -DCMAKE_INSTALL_PREFIX=Doppelganger_install
-    cmake --build build --config "Debug"
-    cmake --install build --config "Debug"
+    subst X: .
+    cd X:
+    cmake -B build -S . -DVCPKG_TARGET_TRIPLET="${TRIPLET}" -DCMAKE_BUILD_TYPE="${CONFIG}" -DCMAKE_INSTALL_PREFIX=Doppelganger_install
+    cmake --build build --config "${CONFIG}"
+    cmake --install build --config "${CONFIG}"
     # revert subst command
     # "/" symbol was comprehended as separator for path in MINGW. Thus, we need to explicitly use "//"
     # echo "unbind ./submodule as X:"
-    # subst X: //D
+    subst X: //D
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    cmake -B build -S . -DVCPKG_TARGET_TRIPLET="${triplet}" -DCMAKE_BUILD_TYPE="Release" -DCMAKE_INSTALL_PREFIX=Doppelganger_install
-    cmake --build build --config "Release"
-    cmake --install build --config "Release"
+    cmake -B build -S . -DVCPKG_TARGET_TRIPLET="${TRIPLET}" -DCMAKE_BUILD_TYPE="${CONFIG}" -DCMAKE_INSTALL_PREFIX=Doppelganger_install
+    cmake --build build --config "${CONFIG}"
+    cmake --install build --config "${CONFIG}"
 else
     echo "This OS is not supported..."
     exit 1
