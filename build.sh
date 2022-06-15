aab39ad6a3d16ca96b846ed8b128725d332c4128
Kazutaka Nakashima <kazutaka.nakashima@n-taka.info>
2022-06-15 17:25:02 +0900

    [misc] tweak build.sh

--- build.sh
+++ build.sh
@@ -23,17 +23,17 @@ fi
 ############
 CONFIG="Release"
 if [ "$(uname)" == "Darwin" ]; then
-    cmake -B build -S . -DVCPKG_TARGET_TRIPLET="${TRIPLET}" -DCMAKE_BUILD_TYPE="${CONFIG}"
+    cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE="submodule/Doppelganger_Util/submodule/vcpkg/scripts/buildsystems/vcpkg.cmake" -DVCPKG_TARGET_TRIPLET="${TRIPLET}" -DCMAKE_BUILD_TYPE="${CONFIG}"
     cmake --build build --config "${CONFIG}"
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
     subst X: submodule/Doppelganger_Util/submodule
-    cmake -B build -S . -DVCPKG_TARGET_TRIPLET="${TRIPLET}" -DCMAKE_BUILD_TYPE="${CONFIG}"
+    cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE="X:/vcpkg/scripts/buildsystems/vcpkg.cmake" -DVCPKG_TARGET_TRIPLET="${TRIPLET}" -DCMAKE_BUILD_TYPE="${CONFIG}"
     cmake --build build --config "${CONFIG}"
     # revert subst command
     # "/" symbol was comprehended as separator for path in MINGW. Thus, we need to explicitly use "//"
     subst X: //D
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    cmake -B build -S . -DVCPKG_TARGET_TRIPLET="${TRIPLET}" -DCMAKE_BUILD_TYPE="${CONFIG}"
+    cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE="submodule/Doppelganger_Util/submodule/vcpkg/scripts/buildsystems/vcpkg.cmake" -DVCPKG_TARGET_TRIPLET="${TRIPLET}" -DCMAKE_BUILD_TYPE="${CONFIG}"
     cmake --build build --config "${CONFIG}"
 else
     echo "This OS is not supported..."
